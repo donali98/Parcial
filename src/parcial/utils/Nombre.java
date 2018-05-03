52da5494f1a7fe87a805a062b1d513fb06cdfd65
Alex7498-SV <36484792+Alex7498-SV@users.noreply.github.com>
2018-05-03 12:50:27 -0600

    creacion de la clase Nombre con sus variables y metodos setter y getter.

---
+++ src/parcial/utils/Nombre.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package parcial.utils;
+
+import java.util.Scanner;
+
+public class Nombre {
+    
+     private  String nombres, apellidos;
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+     
+}
