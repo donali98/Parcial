4182ede643d6b70bf65e956cfd04881e4328b2a8
Alex7498-SV <36484792+Alex7498-SV@users.noreply.github.com>
2018-05-03 13:31:27 -0600

    creacion de la clase huesped que hereda de persona, creacion del constructor sobrecargado de persona

---
+++ src/parcial/base/Huesped.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package parcial.base;
+
+
+public class Huesped extends Persona{
+    
+    public Huesped(String nombre, String apellido, String dui) {
+        super(nombre, apellido, dui);
+    }
+}
