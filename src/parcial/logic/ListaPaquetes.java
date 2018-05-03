ebf998d314a9dc345f60232c5228d5eb932b7263
Donali <00084417@uca.edu.sv>
2018-05-03 16:03:36 -0600

    Logica para lista paquetes

--- src/parcial/logic/ListaPaquetes.java
+++ src/parcial/logic/ListaPaquetes.java
@@ -19,5 +19,57 @@ public class ListaPaquetes {
         return listaPaquetes;
     }
 
+    public  ArrayList<Paquete> getPaquetes(){
+        return paquetes;
+    }
+
+    public  void addPaquete(String nombrePaquete, int cantiServicios){
+        paquetes.add(new Paquete(nombrePaquete,cantiServicios));
+    }
+
+
+    public void addServicioPaquete(Paquete paquete,String descripcion ){
+        paquete.addNewServicio(descripcion);
+    }
+
+    public  void updatePaquete(String nombrePaquete){
+        Scanner scanner = new Scanner(System.in);
+        for(Paquete paquete: paquetes){
+            if(paquete.getNombre().contains(nombrePaquete)){
+                int op = 10;
+                while (op!=0){
+                    Menu.getInstance().crearMenu(new String[]{"1-Cambiar nombre","2-Modificar servicios","0-Volver"});
+                    op = scanner.nextInt();
+                    switch (op){
+                        case 1:
+                            System.out.println("Ingrese el nuevo nombre: ");
+                            paquete.setNombre(scanner.next());
+                            break;
+                        case 2:
+                            boolean encontrado = false;
+                            paquete.listarServicios();
+                            System.out.println("Seleccione el nombre del servicio que desea modificar (0 para cancelar)");
+                            int pa = scanner.nextInt();
+                            while (true){
+                                for (Servicio servicio: paquete.getServicios()){
+                                    if(servicio.getId()== pa){
+                                        encontrado = true;
+                                        servicio.setDescripcion(Servicio.pedir());
+                                        System.out.println("Servicio modificado con exito");
+                                        break;
+                                    }
+                                }
+                                break;
+                            }
+                            if(!encontrado) System.out.println("Servicio no encontrado");
+                            break;
+                        case 0:
+                            Menu.getInstance().menuPaquete();
+                            break;
+                    }
+                }
+            }
+        }
+    }
 
 }
