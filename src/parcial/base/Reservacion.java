9eb01de5b6533a6f47f99f083284ee6c3c439b7c
hugoriv11 <00161417@uca.edu.sv>
2018-05-03 15:59:52 -0600

    Creacion de clase Reservacion

---
+++ src/parcial/base/Reservacion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial.base;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class Reservacion {
+    private Huesped huesped;
+    private Habitacion habitacion;
+    private Paquete paquete;
+    private LocalDate fechaInicio;
+    private  LocalDate fechaFin;    
+}
