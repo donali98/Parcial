6ae8761743a4fff19e7ede9c6a35f6e588a2ed53
Donali <00084417@uca.edu.sv>
2018-05-03 16:01:34 -0600

    Molde de la clase lista paquetes creada

---
+++ src/parcial/logic/ListaPaquetes.java
@@ -0,0 +1,13 @@
+package parcial.logic;
+
+import parcial.base.Paquete;
+import parcial.base.Servicio;
+import parcial.utils.Menu;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ListaPaquetes {
+
+
+
+}
