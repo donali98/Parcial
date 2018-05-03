a6482c2b1c9e1ffc67ca7b37f330cb394dd673cc
hugoriv11 <00161417@uca.edu.sv>
2018-05-03 16:30:21 -0600

    cambio nombre de usuarios

--- src/parcial/utils/Globals.java
+++ src/parcial/utils/Globals.java
@@ -2,7 +2,7 @@ package parcial.utils;
 
 public class Globals {
 
-    public static final String USUARIO_ADMIN = "administrador";
-    public static final String USUARIO_RECEP = "recepcionista";
+    public static final String USUARIO_ADMIN = "admin";
+    public static final String USUARIO_RECEP = "recep";
 
 }
