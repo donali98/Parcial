0ca50487bbb8ed0b72cfbb6776d7291e02c9fb9c
GuillermoMorales <00011417@uca.edu.sv>
2018-05-03 23:52:23 -0600

    Clave

---
+++ src/parcial/utils/Clave.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial.utils;
+
+/**
+ *
+ * @author Guille
+ */
+public class Clave {
+     private String pass;
+
+    public Clave(String pass) {
+        this.pass = pass;
+    }
+
+    public static boolean validarClave(String cadena){
+        return true;
+    }
+}
