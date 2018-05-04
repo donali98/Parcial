486d1da24e2cbf90e271c3655a85b5aa1a7ca1c4
Alex7498-SV <00068817@uca.edu.sv>
2018-05-03 23:10:33 -0600

    importacion de librerias y clases, declaracion de objetos no inicializados y variables

--- src/parcial/logic/ListaHabitacion.java
+++ src/parcial/logic/ListaHabitacion.java
@@ -1,7 +1,16 @@
 package parcial.logic;
 
-
+import parcial.base.Habitacion;
+import parcial.utils.Precios;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ListaHabitacion {
-
+    
+    private static ArrayList<Habitacion> listaHabitaciones;
+    private static ListaHabitacion listaHabitacion;
+    private static int[] valoresUnicode;
+    private static int cantiPisos;
+    private ListaHabitacion(){}
+    
 }
