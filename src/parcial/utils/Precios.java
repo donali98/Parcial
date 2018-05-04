8f6ccaad43800b43c601a48366ef31225e3c9804
hugoriv11 <00161417@uca.edu.sv>
2018-05-03 22:22:10 -0600

    Metodo pedir precios de habitacion sencilla y habitacion doble

--- src/parcial/utils/Precios.java
+++ src/parcial/utils/Precios.java
@@ -25,4 +25,36 @@ public class Precios {
     public static Double getPrecioDoble() {
         return precioDoble;
     }
+    
+    public static void pedirPrecios(){
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            try{
+                System.out.println("Ingrese el precio de la habitacion sencilla: ");
+                precioSencilla = scanner.nextDouble();
+                break;
+
+            }
+            catch (Exception e){
+                scanner.next();
+                System.out.println("Valor ingresado no valido\n");
+            }
+        }
+        while (true){
+            try{
+                System.out.println("Ingrese el precio de la habitacion doble: ");
+                precioDoble = scanner.nextDouble();
+                if(precioSencilla>=precioDoble){
+                    System.out.println("Atencion, el precio de la habitacion sencilla es igual o superior a la doble, desea continuar?(s/n)");
+                    if(scanner.next().contains("s")) break;
+                }
+                else break;
+
+            }
+            catch (Exception e){
+                scanner.next();
+                System.out.println("Valor ingresado no valido\n");
+            }
+        }
+    }    
 }
