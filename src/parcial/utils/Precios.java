1ae314794c381b53fb88f75b608866995a91e5db
hugoriv11 <00161417@uca.edu.sv>
2018-05-03 22:17:01 -0600

    constru de precios y getters

---
+++ src/parcial/utils/Precios.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial.utils;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class Precios {
+    
+    private static Double precioSencilla;
+    private static Double precioDoble;
+
+    private Precios(){}
+
+    public static Double getPrecioSencilla() {
+        return precioSencilla;
+    }
+
+    public static Double getPrecioDoble() {
+        return precioDoble;
+    }
+}
