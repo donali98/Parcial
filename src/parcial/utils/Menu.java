d720704b8750d760fe8316b9fb6058864c73f117
GuillermoMorales <00011417@uca.edu.sv>
2018-05-03 20:46:14 -0600

    Metodo de la clase main

--- src/parcial/utils/Menu.java
+++ src/parcial/utils/Menu.java
@@ -5,6 +5,8 @@
  */
 package parcial.utils;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Guille
@@ -19,5 +21,39 @@ private static Menu menu;
             menu = new Menu();
         }
         return menu;
+    }
+    //Metodo del menu principal
+    public void menuPrincipal(){
+        int selected = 10;
+        Scanner reader = new Scanner(System.in);
+        while (selected!=0){
+
+            try {
+
+                this.crearMenu(new String[]{"1-Gestionar personal","2-Gestionar hotel","0-Salir del sistema"});
+                selected = reader.nextInt();
+                switch (selected){
+                    case 1:
+                        this.menuEmpleados();
+                        selected = 0;
+                        break;
+                    case 2:
+                        this.menuHotel();
+                        selected = 0;
+                        break;
+                    case 0:
+                        System.out.println("Saliendo del sistema");
+                        break;
+                    default:
+                        System.out.println("No ha ingresado una opcion valida");
+                        break;
+                }
+            }
+            catch (Exception e){
+                reader.next();
+                System.out.println("Valor no valido");
+            }
+
+        }
     }    
 }
